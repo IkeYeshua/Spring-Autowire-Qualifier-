163b2bdd34b81e3689739dd02973c6b69728714e
Okeke Ikernna <ikennaokekeke45@gamil.com>
2023-09-20 14:31:02 +0100

    Dependency injection via autowire annotation

--- src/main/java/autowired/Human.java
+++ src/main/java/autowired/Human.java
@@ -1,16 +1,15 @@
 package autowired;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
 public class Human {
     private Heart heart;
 
 
-    public Human(){
-
-    }
-
+    public Human(){}
+@Autowired
    public Human(Heart heart) {
-       this.heart = heart;
-    }
+      this.heart = heart;}
 
     public void setHeart(Heart heart) {
         this.heart = heart;
