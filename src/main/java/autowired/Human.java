8da6f565145cdca26267e478e7dfa20ae9818759
Okeke Ikernna <ikennaokekeke45@gamil.com>
2023-09-20 14:03:22 +0100

    Dependency injection via autowire by constructor

--- src/main/java/autowired/Human.java
+++ src/main/java/autowired/Human.java
@@ -8,9 +8,9 @@ public class Human {
 
     }
 
-   // public Human(Heart heart) {
-    //    this.heart = heart;
- //   }
+   public Human(Heart heart) {
+       this.heart = heart;
+    }
 
     public void setHeart(Heart heart) {
         this.heart = heart;
