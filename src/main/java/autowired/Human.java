329f57ece28b543b158fee7a162d707e4298b75a
Okeke Ikernna <ikennaokekeke45@gamil.com>
2023-09-20 13:53:55 +0100

    Dependency injection via autowire by name

--- src/main/java/autowired/Human.java
+++ src/main/java/autowired/Human.java
@@ -3,6 +3,15 @@ package autowired;
 public class Human {
     private Heart heart;
 
+
+    public Human(){
+
+    }
+
+   // public Human(Heart heart) {
+    //    this.heart = heart;
+ //   }
+
     public void setHeart(Heart heart) {
         this.heart = heart;
     }
