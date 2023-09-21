55c40f93f9dfa0cf00a6da9b697e7f9dd2e2ec85
Okeke Ikernna <ikennaokekeke45@gamil.com>
2023-09-21 05:38:57 +0100

    spring core: decalring the @Autowire and @Qualifier before the dependency

--- src/main/java/autowired/Human.java
+++ src/main/java/autowired/Human.java
@@ -4,18 +4,18 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 
 public class Human {
+    @Autowired
+    @Qualifier("octopusHeart")
     private Heart heart;
 
 
-    public Human(){}
+  //  public Human(){}
 
-   public Human(Heart heart) {
-      this.heart = heart;}
-@Autowired
-@Qualifier("octopusHeart")
-    public void setHeart(Heart heart) {
-        this.heart = heart;
-    }
+   //public Human(Heart heart) {
+     // this.heart = heart;}
+    //public void setHeart(Heart heart) {
+      //  this.heart = heart;
+   // }
 
     public void startPumping() {
 
